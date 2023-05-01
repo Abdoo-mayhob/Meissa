b0f4ecd6e072943b85f6e55acd6bc1d5fb389802
Abdoo-mayhob <abdoo.mayhob@gmail.com>
2023-05-01 23:19:27 +0300

    Removed wp-emoji js

--- inc/theme-setup.php
+++ inc/theme-setup.php
@@ -115,6 +115,28 @@ function meissa_remove_wp_bloat(){
 }
 
 // ------------------------------------------------------
+// Remove wp-emoji
+add_action( 'init', 'meissa_remove_wp_emoji' );
+function meissa_remove_wp_emoji() {
+ remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+ remove_action( 'wp_print_styles', 'print_emoji_styles' );
+ remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+ remove_action( 'admin_print_styles', 'print_emoji_styles' );
+ remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+ remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+ add_filter( 'tiny_mce_plugins', 'meissa_disable_emojis_tinymce' );
+}
+function meissa_disable_emojis_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+        return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+        return [];
+    }
+}
+
+
+// ------------------------------------------------------
 // Defer all Scripts (including jquery)
 add_filter( 'script_loader_tag', function ( $tag, $handle ) {
     return str_replace( ' src', ' defer="defer" src', $tag );
